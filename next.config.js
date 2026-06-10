f392e75a727cfcd9476ee7531e90503b73566905
frassetu <frassetu@gmail.com>
2026-06-10 11:22:19 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
 
-module.exports = nextConfig
-``
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = withPWA(nextConfig)
