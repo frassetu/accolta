0c1a1fc93f4d73df9efccb91aa4b4456ef018815
frassetu <frassetu@gmail.com>
2026-07-04 22:21:24 +0200

    Add files via upload

---
+++ src/components/Splash.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useEffect } from 'react'
+
+interface Props {
+  onFinish: () => void
+}
+
+// Durée d'affichage de l'écran de lancement avant de basculer sur l'appli.
+const SPLASH_DURATION_MS = 1300
+
+export default function Splash({ onFinish }: Props) {
+  useEffect(() => {
+    const timer = setTimeout(onFinish, SPLASH_DURATION_MS)
+    return () => clearTimeout(timer)
+  }, [onFinish])
+
+  return (
+    <div className="fixed inset-0 z-[100] bg-black flex flex-col items-center justify-center gap-6 fade-in">
+      <img src="/icon-192.png" alt="" className="w-20 h-20 rounded-2xl" />
+      <img src="/logo-wordmark.png" alt="Vogliu Cantà !" className="w-full max-w-[280px] h-auto px-10" />
+    </div>
+  )
+}
